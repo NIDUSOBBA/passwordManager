75979e8985ec5a151279f21531c9767b42286c22
aubba <abublikbrazers@gmail.com>
2026-07-07 17:56:19 +0300

    feat: сеттер для WindowManager

--- src/main/java/org/example/panel/BaseMethod.java
+++ src/main/java/org/example/panel/BaseMethod.java
@@ -1,9 +1,12 @@
 package org.example.panel;
 
 
+import org.example.controller.WindowManager;
+
 public interface BaseMethod {
 
     void loadTable();
     void add();
     void delete();
+    void setWindowManager(WindowManager windowManager);
 }
