fee26211bf8d246ee90565616d8e0c94e9ed4c1f
aubba <abublikbrazers@gmail.com>
2026-07-16 14:47:48 +0300

    refactor: Исправил работу сортировщика

--- src/main/java/org/example/utile/DefaultModel.java
+++ src/main/java/org/example/utile/DefaultModel.java
@@ -6,8 +6,17 @@ public class DefaultModel {
 
     public static DefaultTableModel creteModel(String[] cols){
         return new DefaultTableModel(cols, 0) {
+            @Override
             public boolean isCellEditable(int r, int c) {
                 return false;
+            }
+            //Говорим сортировщику: "Сортируй как числа!" а остальное как объект
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                if (columnIndex == 0) {
+                    return Integer.class; //
+                }
+                return Object.class;
             }
         };
     }
